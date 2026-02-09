340d922008db367e735c4a1391765dd820e07dd7
santivgo <requiemsantiago@gmail.com>
2026-02-09 11:50:27 -0300

    feat: initializing the project

---
+++ src/main/java/com/sant/toomanymovies/ToomanymoviesApplication.java
@@ -0,0 +1,13 @@
+package com.sant.toomanymovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToomanymoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToomanymoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sant/toomanymovies/ToomanymoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sant.toomanymovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToomanymoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
